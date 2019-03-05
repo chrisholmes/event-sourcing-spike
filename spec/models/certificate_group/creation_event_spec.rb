3d2115868b6d4425c6495d0fabe2b235d33b57ed
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2019-03-05 09:10:14 +0000

    move shared examples to a new location

--- spec/models/certificate_group/creation_event_spec.rb
+++ spec/models/certificate_group/creation_event_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'models/event_spec'
 
 RSpec.describe CertificateGroup::CreationEvent, type: :model do
   include_examples "is a creation event", CertificateGroup::CreationEvent, {}
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,9 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
 
+# Load shared examples
+Dir[Rails.root.join("spec/models/shared_examples/**/*.rb")].each {|f| require f}
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
